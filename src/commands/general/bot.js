3fdc9a8f9aa47b357d4247d920ee3e4fd0196d88
kurozeropb <pepijn.vanden.broek@gmail.com>
2018-02-05 00:51:49 +0100

    owo

--- src/commands/general/bot.js
+++ src/commands/general/bot.js
@@ -16,8 +16,9 @@ class Bot extends Command {
 
     async handle({msg, client, rawArgs}, responder) {
         if (!discordpw_key) return;
+        let user, data, inv, owners;
         if (msg.mentions[0]) {
-            const user = msg.mentions[0];
+            user = msg.mentions[0];
             if (user.bot === false) return responder.send('❌ This is not a bot.');
             const resp = await axios.get(`https://bots.discord.pw/api/bots/${user.id}`, {
                 headers: {
@@ -25,29 +26,14 @@ class Bot extends Command {
                     'User-Agent': USERAGENT
                 }
             });
-            const data = resp.data;
+            data = resp.data;
             if (resp.status !== 200) return responder.send(`Could not fetch the data,\n${resp.status}: ${resp.message}`);
-            const inv = data.invite_url.replace(/ /g, '%20');
-            const owners = data.owner_ids.map((o) => `<@!${o}>`);
-            responder.send('', {
-                embed: {
-                    color: client.utils.getDefaultColor(msg, client),
-                    author: {name: data.name, url: data.website, icon_url: ``},
-                    thumbnail: {url: `${user.avatarURL}`},
-                    description: `**ID:** ${data.client_id}\n` +
-                    `**Desc:** ${data.description}\n` +
-                    `**Library:** ${data.library}\n` +
-                    `**Owners:** ${owners}\n` +
-                    `**Prefix:** ${data.prefix}\n` +
-                    `**Invite:** [\`Click here\`](${inv})\n` +
-                    `**Website:** ${data.website}`,
-                    footer: {text: `Data from bots.discord.pw`, icon_url: ``}
-                }
-            });
+            inv = data.invite_url.replace(/ /g, '%20');
+            owners = data.owner_ids.map((o) => `<@!${o}>`);
         } else {
             const idRegex = /^\d{17,18}$/.test(rawArgs[0]);
             if (idRegex === false) return 'wrong usage';
-            const user = client.users.get(rawArgs[0]);
+            user = client.users.get(rawArgs[0]);
             if (!user) return responder.send('❌ Something went wrong, make sure it\'s a valid user.');
             if (user.bot === false) return responder.send('❌ This is not a bot.');
             const resp = await axios.get(`https://bots.discord.pw/api/bots/${user.id}`, {
@@ -56,26 +42,26 @@ class Bot extends Command {
                     'User-Agent': USERAGENT
                 }
             });
-            const data = resp.data;
+            data = resp.data;
             if (resp.status !== 200) return responder.send(`Could not fetch the data,\n${resp.status}: ${resp.message}`);
-            const inv = data.invite_url.replace(/ /g, '%20');
-            const owners = data.owner_ids.map((o) => `<@!${o}>`);
-            responder.send('', {
-                embed: {
-                    color: client.utils.getDefaultColor(msg, client),
-                    author: {name: data.name, url: data.website, icon_url: ``},
-                    thumbnail: {url: `${user.avatarURL}`},
-                    description: `**ID:** ${data.client_id}\n` +
-                    `**Desc:** ${data.description}\n` +
-                    `**Library:** ${data.library}\n` +
-                    `**Owners:** ${owners}\n` +
-                    `**Prefix:** ${data.prefix}\n` +
-                    `**Invite:** [\`Click here\`](${inv})\n` +
-                    `**Website:** ${data.website}`,
-                    footer: {text: `Data from bots.discord.pw`, icon_url: ``}
-                }
-            });
+            inv = data.invite_url.replace(/ /g, '%20');
+            owners = data.owner_ids.map((o) => `<@!${o}>`);
         }
+        responder.send('', {
+            embed: {
+                color: client.utils.getDefaultColor(msg, client),
+                author: {name: data.name, url: data.website, icon_url: ''},
+                thumbnail: {url: `${user.avatarURL}`},
+                description: `**ID:** ${data.client_id}\n` +
+                `**Desc:** ${data.description}\n` +
+                `**Library:** ${data.library}\n` +
+                `**Owners:** ${owners}\n` +
+                `**Prefix:** ${data.prefix}\n` +
+                `**Invite:** [\`Click here\`](${inv})\n` +
+                `**Website:** ${data.website}`,
+                footer: {text: `Data from bots.discord.pw`, icon_url: ''}
+            }
+        });
     }
 }
 
